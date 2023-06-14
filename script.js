eb2349349f138fc3a5584e7762aedae0df45602d
krushikar543 <77056197+krushikar543@users.noreply.github.com>
2023-06-14 12:10:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,62 @@
-
-function submit(){
+function username_check(){
     var first = document.getElementById("first");
-    // var last = document.getElementById("last");
-    var gender = document.getElementById("gender");
-    var country = document.getElementById("country");
-    var phno = document.getElementById("phno");
-    var email = document.getElementById("exampleFormControlInput1");
-    var password = document.getElementById("pass");
-    var pass_str = document.getElementById("password_feedback");
-    var confirm_password = document.getElementById("con_pass");
-    var error = document.getElementById("errors");
-    var check = 0;
-    error.innerHTML = "";
-    error.classList.remove('errordiv');
-    error.classList.remove('correctdiv');
- 
-    // console.log(error);
+    var error = document.getElementById("error_name");
     if(first.value.length < 5){
-        error.innerHTML += "UseName must have atleast 5 characters. <br>";
+        error.innerHTML = "UseName must have atleast 5 characters. <br>";
         first.classList.add('clicked');
     }
     else{
         first.classList.remove('clicked');
-        check+=1;
+        error.innerHTML = "";
     }
+}
 
-
+function country_field(){
+    var country = document.getElementById("country");
+    var error = document.getElementById("error_country");
     if(country.value.length === 0){
-        error.innerHTML += "Country field should not be left empty.<br>";
+        error.innerHTML = "Country field should not be left empty.<br>";
         country.classList.add('clicked');
     }
     else{
         country.classList.remove('clicked');
-        check += 1;
+        error.innerHTML = "";
+        
     }
+    
+}
 
+function phno_cond(){
+    var phno = document.getElementById("phno");
+    var error = document.getElementById("error_phno");
     if(phno.value.length !== 10){
-        // alert("Phone Number must have 10 digits");
-        error.innerHTML += "Phone Number must contain 10 digits.<br>";
+        error.innerHTML = "Phone Number must contain 10 digits.<br>";
         phno.classList.add('clicked');
     }
     else{
         phno.classList.remove('clicked');
-        check+=1;
+        error.innerHTML = "";
     }
+}
 
-
+function email_check(){
+    var email = document.getElementById("exampleFormControlInput1");
+    var error = document.getElementById("error_email");
     if(!email.value.includes("@")){
-        // alert("Email Id should contain '@'. Please enter a valid Email Id.");
-        error.innerHTML += "Email Id should contain '@'. Please enter a valid Email Id.<br>";
+        error.innerHTML = "Email Id should contain '@'. Please enter a valid Email Id.<br>";
         email.classList.add("clicked");
     }
     else{
         email.classList.remove("clicked");
-        check+=1;
+        error.innerHTML = "";
     }
 
-    
+}
 
+function pass_strength_check(){
+    var password = document.getElementById("pass");
+    var error = document.getElementById("error_pass");
+    var errors = document.getElementById("feedback");
 
     var strength = 0;
     var regex = new Array();
@@ -73,76 +72,101 @@ function submit(){
     }
     switch (strength) {
       case 0:
-        pass_str.classList.add('pass_weak');
-        pass_str.classList.remove('pass_strong');
-        pass_str.innerHTML = "Your Password is Very Weak";
+        error.classList.add('pass_weak');
+        error.classList.remove('pass_strong');
+        error.innerHTML = "Your Password is Very Weak";
         break;
       case 1:
-        pass_str.classList.add('pass_weak');
-        pass_str.classList.remove('pass_strong');
-        pass_str.innerHTML = "Your Password is Weak";
+        error.classList.add('pass_weak');
+        error.classList.remove('pass_strong');
+        error.innerHTML = "Your Password is Weak";
         break;
       case 2:
-        pass_str.classList.add('pass_weak');
-        pass_str.classList.remove('pass_strong');
-        pass_str.innerHTML = "Your Password is Moderate";
+        error.classList.add('pass_weak');
+        error.classList.remove('pass_strong');
+        error.innerHTML = "Your Password is Moderate";
         break;
       case 3:
-        pass_str.classList.add('pass_strong');
-        pass_str.classList.remove('pass_weak');
-        pass_str.innerHTML = "Your Password is Strong";
+        error.classList.add('pass_strong');
+        error.classList.remove('pass_weak');
+        error.innerHTML = "Your Password is Strong";
         break;
       case 4:
-        pass_str.classList.add('pass_strong');
-        pass_str.classList.remove('pass_weak');
-        pass_str.innerHTML = "Your Password is Very Strong";
+        error.classList.add('pass_strong');
+        error.classList.remove('pass_weak');
+        error.innerHTML = "Your Password is Very Strong";
         break;
     }
     
 
 
     if(password.value.length < 8){
-        error.innerHTML += "Password should contain atleast 8 characters.<br>";
+        errors.innerHTML = "Password should contain atleast 8 characters.<br>";
         password.classList.add('clicked');
     }
 
     else if(strength < 3){
-        // alert("Avoid using easily predictable passwords.Plase change your password");
-        error.innerHTML += "Your Password is weak. Please change your password.<br>";
+        errors.innerHTML = "Your Password is weak. Please change your password.<br>";
         password.classList.add('clicked');
     }
     else{
         password.classList.remove('clicked');
-        check+=1;
+        errors.innerHTML = "";
     }
+}
 
-    if(confirm_password.value !== password.value){
-        // alert("Password must match with confirm password");
-        error.innerHTML += "Confirm Password must match with Password.<br>";
-        confirm_password.classList.add('clicked');
+function submit(){
+    var first = document.getElementById("first");
+    var gender = document.getElementById("gender");
+    var country = document.getElementById("country");
+    var phno = document.getElementById("phno");
+    var email = document.getElementById("exampleFormControlInput1");
+    var password = document.getElementById("pass");
+    var pass_str = document.getElementById("password_feedback");
+    var confirm_password = document.getElementById("con_pass");
+    var error = document.getElementById("errors");
+    error.innerHTML = "";
+    error.classList.remove('errordiv');
+    error.classList.remove('correctdiv');
+
+    // error fields
+    var error_name = document.getElementById("error_name");
+    var error_country = document.getElementById("error_country");
+    var error_phno = document.getElementById("error_phno");
+    var error_email = document.getElementById("error_email");
+    var error_pass = document.getElementById("feedback");
+    var pass_str = document.getElementById("error_pass");
+
+    var error_val = 0;
+    if(error_name.innerHTML === "" && error_country.innerHTML === "" &&  error_phno.innerHTML === "" &&  error_email.innerHTML === "" &&  error_pass.innerHTML === ""){
+        error_val += 1;
     }
     else{
-        confirm_password.classList.remove('clicked');
-        check+=1;
+        error.innerHTML += "Fill the Details correctly<br>";
+        error.classList.add('errordiv');
     }
 
 
-
-
-
-
-
-    if(check === 6){
+    if(confirm_password.value !== password.value){
+        error.innerHTML += "Confirm Password must match with Password.<br>";
+        confirm_password.classList.add('clicked');
+        error.classList.add('errordiv');
+    }
+    else{
+        error_val += 1;
+    }
+    if(error_val === 2){
+        confirm_password.classList.remove('clicked');
         first.value = "";
         country.value = "";
         phno.value = "";
         email.value = "";
         password.value = "";
         confirm_password.value = "";
+        pass_str.innerHTML = "";
         error.innerHTML = "Submitted !!!<br>Reload Page to make another submission.";
         error.classList.add('correctdiv');
+        
     }
-    else{
-        error.classList.add('errordiv');
-    }
+
 }
